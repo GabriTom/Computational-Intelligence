ab028758ab09a5f26c0e9f8a41f00cf26b231452
GabriTom <gabriele.tomatis99@gmail.com>
2023-11-17 21:02:29 +0100

    Lab2 finished

--- Lab2/only_for_debug.py
+++ Lab2/only_for_debug.py
@@ -5,7 +5,8 @@ import random
 from copy import deepcopy
 
 Nimply = namedtuple("Nimply", "row, num_objects")
-N_MATCHES = 10
+N_MATCHES = 30
+N_TRAINS = 50
 
 class Nim:
     def __init__(self, num_rows: int, k: int = None) -> None:
@@ -108,55 +109,101 @@ def optimal(state: Nim) -> Nimply:
 
 
 
-MAX_PROB = 10
+MAX_PROB = 100
 
-def weighted_random(state: Nim) -> Nimply:
-    solutions = []
     
 
-    for alfa in range(1, MAX_PROB):
-        for beta in range(1, MAX_PROB):
+def weighted_random(state: Nim, alfa, beta) -> Nimply:
+    """A weighted random move"""
+    idx = abs(int(alfa/MAX_PROB))
+    row = [r for r, c in enumerate(state.rows) if c > 0]
+    
+    # Avoiding out of bound in rows
+    if idx >= len(row):
+        row = row[-1]
+    else:
+        row = row[idx]
 
-            
-            """A weighted random move"""
-            row = round((alfa/MAX_PROB)*len(state._rows))
-            num_objects = round((beta/MAX_PROB)*state.rows[row])
-
-            #Come fai qua a simulare la partita dato che hai una sola strategia????
-            
-            #----- random approach enhancements (including k variant) -----
-            #Forcing k
-            if num_objects >= state._k:
-                num_objects = state._k
-            
-
-
+    num_objects = int(np.round((beta/MAX_PROB)*random.randint(1, state.rows[row]+1)))
+    #Forcing a suitable num_object regarding all constraints
+    if num_objects <= 0:
+        num_objects = 1
+    else:
+        if num_objects > state._k:
+            num_objects = state._k
+        if num_objects > state.rows[row]:
+            num_objects = state.rows[row]
+ 
     return Nimply(row, num_objects)
+
+
+
 
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
-
     strategy = (optimal, weighted_random)
-    p_wins = [0, 0]
-    i = 0
 
-    #Add external loop generating other parameters for the ES + and , algorithm
-    while(i < N_MATCHES):
-        #alfa_best = 
-        #ùbeta_best = 
-        # fatti restituire prossima mossa per un set di parametri
-        # nel caso aggiorna i best, variali segiuendo gaussiana 
-        #while(j < N_TRAINS):
+    alfa = random.randint(0, MAX_PROB)
+    beta = random.randint(0, MAX_PROB)
+    # sigma chosen randomly
+    sigma = random.randint(0, MAX_PROB)
+
+    alfa_best = alfa
+    beta_best = beta
+    wins_best = 0
+
+    for _ in range(0, N_TRAINS):
+        p_wins = [0, 0]
+        print(f"alfa_b: {alfa_best}, beta_best: {beta_best}")
+        #Executing N_MATCHES fro alfa and beta parameters
+        for _ in range(0, N_MATCHES):
+            nim = Nim(5, 4)
+            #logging.info(f"init : {nim}")
+            player = 0
+            while nim:
+                if player == 0:
+                    ply = strategy[player](nim)
+                else:
+                    ply = strategy[player](nim, alfa_best, beta_best)
+                #logging.info(f"ply: player {player} plays {ply}")
+                nim.nimming(ply)
+                #logging.info(f"status: {nim}")
+                player = 1 - player
+            #logging.info(f"----------------------------     status: Player {player} won!      ----------------------------")
+            p_wins[player] += 1
+
+        print(f"BEST WINS: {wins_best} \tACTUAL WINS: {p_wins[1]}")
+            
+        #  --- PLUS STRATEGY ---
+        #If this model scored better than the previous, model evolves from actual parameters
+        if wins_best < p_wins[1]:
+            wins_best = p_wins[1]
+            alfa_best = round(np.random.normal(alfa, sigma))
+            beta_best = round(np.random.normal(beta, sigma))
+        else:
+            alfa_best = round(np.random.normal(alfa_best, sigma))
+            beta_best = round(np.random.normal(beta_best, sigma))
+
+        #  --- COMMA STRATEGY ---
+        #Model evolves from actual parameters
+        #alfa_best = round(np.random.normal(alfa, sigma))
+        #beta_best = round(np.random.normal(beta, sigma))
+
+    p_wins = [0, 0]
+    # Doing last N_MATCHES with trained values
+    for _ in range(0, N_MATCHES):
         nim = Nim(5, 4)
         logging.info(f"init : {nim}")
         player = 0
         while nim:
-            ply = strategy[player](nim)
+            if player == 0:
+                ply = strategy[player](nim)
+            else:
+                ply = strategy[player](nim, alfa_best, beta_best)
             logging.info(f"ply: player {player} plays {ply}")
             nim.nimming(ply)
             logging.info(f"status: {nim}")
             player = 1 - player
         logging.info(f"----------------------------     status: Player {player} won!      ----------------------------")
         p_wins[player] += 1
-        i+=1
     print(p_wins)
