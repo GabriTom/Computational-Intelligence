50e2411c2114feec53ecd0df4279bb29fcd9890c
GabriTom <gabriele.tomatis99@gmail.com>
2023-12-14 00:04:21 +0100

    Small changes

--- Lab10/just_for_debug.py
+++ Lab10/just_for_debug.py
@@ -1,6 +1,12 @@
 import numpy as np
 import random
 
+class game:
+    def __init__(self):
+        self.state = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        self.possible_moves = 9
+
+
 SOL_REF = [[4, 9, 2], [3, 5, 7], [8, 1, 6]]
 CMP_VALUE = 15
 N_ROUNDS = 10
@@ -9,61 +15,79 @@ CIRCLE = -1
 CROSS = 1
 
 def init_game():
-    basic_t = [BASIC_VAL, BASIC_VAL, BASIC_VAL]
-    game = [basic_t, basic_t, basic_t]
+    game = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
     return game
 
 def check_finished(state):
     #Checking if cicle won thrpugh summing values and comparing it to the default value to check
-    circle_based_state = -(state * SOL_REF)
+    circle_based_state = -np.multiply(state, SOL_REF)
     win_circle_row = np.sum(circle_based_state, axis=0)
     win_circle_col = np.sum(circle_based_state, axis=1)
     win_circle_diag = circle_based_state[0, 0] + circle_based_state[1, 1] + circle_based_state[2, 2]
+    win_circle_anti_diag = cross_based_state[0, 2] + cross_based_state[1, 1] + cross_based_state[2, 0]
     if CMP_VALUE in win_circle_row:
         return CIRCLE
     if CMP_VALUE in win_circle_col:
         return CIRCLE
     if CMP_VALUE == win_circle_diag:
         return CIRCLE
+    if CMP_VALUE == win_circle_anti_diag:
+        return CIRCLE
     
     #Same thing with cross
-    cross_based_state = (state * SOL_REF)
+    cross_based_state = np.multiply(state, SOL_REF)
     win_cross_row = np.sum(cross_based_state, axis=0)
     win_cross_col = np.sum(cross_based_state, axis=1)
     win_cross_diag = cross_based_state[0, 0] + cross_based_state[1, 1] + cross_based_state[2, 2]
+    win_cross_anti_diag = cross_based_state[0, 2] + cross_based_state[1, 1] + cross_based_state[2, 0]
     if CMP_VALUE in win_cross_row:
         return CROSS
     if CMP_VALUE in win_cross_col:
         return CROSS
     if CMP_VALUE == win_cross_diag:
         return CROSS
+    if CMP_VALUE == win_cross_anti_diag:
+        return CIRCLE
     
     return BASIC_VAL
 
-def new_move(game, current_player):
-    print("vdishber")
+def random_s(state, current_player):
+    row = random.randint(0, 2)
+    col = random.randint(0, 2)
+    while state[row][col] != BASIC_VAL:
+        row = random.randint(0, 2)
+        col = random.randint(0, 2)
 
-def random_s(state):
-    row = random.random(3)
-    col = random.random(3)
-    if state[row, col] == BASIC_VAL:
-        #Metti il simbolo del giocatore corrispondente...come lo sai????
+    if current_player == 0:
+        state[row][col] = CIRCLE
+    else:
+        state[row][col] = CROSS
+
+    return state
 
 def opt_s(state):
     print("3frerc")
 
-PLAYERS = [random_s, opt_s]
+PLAYERS = [random_s, random_s]
 
 if __name__ == "__main__":
-    
-    for _ in range(N_ROUNDS):
+    p_wins = [0, 0]
+    for i in range(N_ROUNDS):
         current_player = 0
-        game = init_game()
-        ending = check_finished(game)
-        while  ending == BASIC_VAL:
-            game = new_move(game, players[current_player])
+        match = game()
+        ending = check_finished(match)
+        
+        while ending == BASIC_VAL and match.possible_moves != 0:
+            match = PLAYERS[current_player](match.state, current_player)
+            match.possible_moves -= 1
             current_player = abs(current_player - 1)
+            ending = check_finished(match.state)
+
+        if match.possible_moves == -1:
+            print("PAREGGIO!")
         if ending == CROSS:
-            print("Cross player won!")
+            p_wins[1] += 1
         else:
-            print("Circle player won!")
+            p_wins[0] += 1
+
+    print(p_wins)
