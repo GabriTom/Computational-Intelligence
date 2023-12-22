a28e30bff9aad30cba102e729a13a0113c140ba8
GabriTom <gabriele.tomatis99@gmail.com>
2023-12-22 16:54:58 +0100

    Q-Learning agent
    Implementaed a q-learning agent that uses the optimal strategy during training and what he had learned during testing phase.

--- Lab10/just_for_debug.py
+++ Lab10/just_for_debug.py
@@ -1,13 +1,12 @@
 import numpy as np
 import random
+from copy import deepcopy
 
-#Tic-tac-toe game solved in a Markovian way
-
-EMPTY_STATE = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 SOL_REF = [[4, 9, 2], [3, 5, 7], [8, 1, 6]]
 CMP_VALUE = 15
-N_ROUNDS = 10
-N_MATCHES = 1
+N_ROUNDS_TRAIN = 2000
+N_ROUNDS_TEST = 10
+N_MATCHES = 10
 BASIC_VAL = 0
 CIRCLE = -1
 CROSS = 1
@@ -19,20 +18,33 @@ class game:
         self.possible_moves = 9
 
 class player:
-    def __init__(self, sign, strategy):
+    def __init__(self, sign):
         self.sign = sign
-        self.strategy = strategy
         self.exp_rate = 0.3
-        if strategy == opt_s:
-            self.optimals = np.zeros((3, 3))
+        self.decay_gamma = 0.9
+        self.lr = 0.2
+        self.optimals = np.zeros((3, 3))
+        self.states = []
+        self.states_values = {}
+        self.training_phase = True
 
-    def reset(self, winnings):
-        self.optimals = np.ones((3, 3))
-        self.optimals[1, 1] = 3
-        # if winnings[1] < winnings[0]:
-        #     self.alpha *= 1.1
-        # else:
-        #     self.alpha *= 0.9
+    def feedReward(self, reward):
+        for st in reversed(self.states):
+            if self.states_values.get(st) is None:
+                self.states_values[st] = 0
+            self.states_values[st] += self.lr * (self.decay_gamma * reward - self.states_values[st])
+            reward = self.states_values[st]
+
+    def reset(self):
+        self.optimals = np.zeros((3, 3))
+
+#Returns a hash of a state (= state in a string)
+def get_hash(state):
+    ret = []
+    for row in state:
+        for cell in row:
+            ret.append(cell)
+    return str(ret)
 
 #Function that receives a state a computes if it is won or not
 def check_finished(state):
@@ -91,7 +103,6 @@ def compute_distance(state, player, available_positions):
         elif e == 2:
             e[0] = e[0] * 2 + 1
     actual_optimals += abs(sum_row)
-    
         
     sum_col = np.sum(state, axis=0).reshape(1, len(state[1]))
     for e in sum_col[0]:
@@ -114,82 +125,135 @@ def compute_distance(state, player, available_positions):
         sum_anti_diag = sum_anti_diag * 2 + 1
     
     for i in range(player.optimals.shape[0]):
-        #actual_optimals[i, i] = round(actual_optimals[i, i] + abs(sum_diag) * player.alpha)
         actual_optimals[i, i] += abs(sum_diag)
 
     for i in range(player.optimals.shape[0]):
-        #actual_optimals[player.optimals.shape[0]-1-i, i] = round(actual_optimals[player.optimals.shape[0]-1-i, i] + abs(sum_anti_diag) * player.alpha)
         actual_optimals[player.optimals.shape[0]-1-i, i] += abs(sum_anti_diag)
 
-    #Scegli cella con abs maggiore
     player.optimals = np.multiply(actual_optimals, available_positions)
     # print("Actual optimals LATER")
     # print(player.optimals)
-    print()
 
-    
 
-#Function that receives a state and a player and do the best move based on optimals values
+# #Function that receives a state and a player and do the best move based on optimals values
 def opt_s(state, current_player):
     available_positions = np.zeros((len(state), len(state[0])))
-    #Analize state (find empty position) and updating optimals
+
+    #Finding empty positions
     for i in range(len(state)):
         for j in range(len(state[i])):
             if state[i][j] == 0:
                 available_positions[i][j] = 1
-
     # print("Available position")
     # print(available_positions)
     compute_distance(state, current_player, available_positions)
 
     #Selecting cell
     sr, sc = np.unravel_index(np.argmax(abs(current_player.optimals), axis=None), current_player.optimals.shape)
-    print((sr, sc))
+    # print((sr, sc))
     state[sr][sc] = current_player.sign
 
     return state
 
+#Function that perform a move accordingly to the player exploration_rate and is the player is in traning phase or not
+def perform_move(state, player):
+    #Only player with cross is a Q-learned agent
+    if player.sign == CIRCLE:
+        move = random_s(state, player)
+    else:
+        available_positions_list = []
+        if np.random.uniform(0, 1) < player.exp_rate:
+            move = random_s(state, player)
+        else:
+            if player.training_phase:
+                move = opt_s(state, player)
+            else:
+                #Creating a list of available positions
+                for i in range(len(state)):
+                    for j in range(len(state[i])):
+                        if state[i][j] == 0:
+                            available_positions_list.append(i*len(state)+j)
+
+                value_max = -999
+                for p in available_positions_list:
+                    next_state = deepcopy(state)
+                    row = p // len(state)
+                    col = p % len(state)
+                    next_state[row][col] = player.sign
+                    next_board_hash = get_hash(next_state)
+                    value = 0 if player.states_values.get(next_board_hash) is None else player.states_values.get(next_board_hash)
+                    # print("value", value)
+                    if value >= value_max:
+                        value_max = value
+                        move = next_state
+
+        player.states.append(get_hash(move))
+
+    return move
+
 
 if __name__ == "__main__":
-    p_wins = [0, 0]
-    p_matches = [0, 0]
-    player_1 = player(CIRCLE, random_s)
-    player_2 = player(CROSS, opt_s)
+    player_1 = player(CIRCLE)
+    player_2 = player(CROSS)
     players = [player_1, player_2]
 
-    #Dopo un tot dai feed back e vari parametri
-    #Potresti definire un epslion che è il peso da sommarte nell'optimal e saeconda del feedback lo vari
-    for _ in range(N_MATCHES):
-        for i in range(N_ROUNDS):
-            current_player = 0
-            match = game()
+    for i in range(N_ROUNDS_TRAIN):
+        current_player = 0
+        match = game()
+        ending = check_finished(match.state)
+        
+        while ending == BASIC_VAL and match.possible_moves != 0:
+            match.state = perform_move(match.state, players[current_player])
+            match.possible_moves -= 1
+            current_player = abs(current_player - 1)
             ending = check_finished(match.state)
-            
-            while ending == BASIC_VAL and match.possible_moves != 0:
-                match.state = players[current_player].strategy(match.state, players[current_player])
-                print("GAME STATE: ")
-                print(match.state)
-            
-                match.possible_moves -= 1
-                current_player = abs(current_player - 1)
-                ending = check_finished(match.state)
-
-            if ending == BASIC_VAL:
-                print("PAREGGIO!")
-            else:
-                if ending == CROSS:
-                    p_wins[1] += 1
-                else:
-                    p_wins[0] += 1
-
-            player_2.reset(p_wins)
-
-        print(p_wins)
-
-        if p_wins[0] > p_wins[1]:
-            p_matches[0] += 1
+        
+        if ending == CROSS:
+            player_2.feedReward(1)
+            player_1.feedReward(0)
+        elif ending != BASIC_VAL:
+            player_2.feedReward(0)
+            player_1.feedReward(1)
         else:
-            p_matches[1] += 1
+            player_1.feedReward(0.5)
+            player_2.feedReward(0.1)
+
+        player_2.reset()
     
-    print(p_matches)
+    player_1.training_phase = False
+    player_2.training_phase = False
+
+    p_wins = [0, 0]
+    p_matches = [0, 0]
+
+    for i in range(N_ROUNDS_TEST):
+        current_player = 0
+        match = game()
+        ending = check_finished(match.state)
+        
+        while ending == BASIC_VAL and match.possible_moves > 0:
+            match.state = perform_move(match.state, players[current_player])
+            match.possible_moves -= 1
+            current_player = abs(current_player - 1)
+            ending = check_finished(match.state)
+
+        if ending == BASIC_VAL:
+            print("DRAW!")
+        elif ending == CROSS:
+            p_wins[1] += 1
+        else:
+            p_wins[0] += 1
+
+    print(p_wins)
+    # if p_wins[0] > p_wins[1]:
+    #     p_matches[0] += 1
+    # elif p_wins[0] < p_wins[1]:
+    #     p_matches[1] += 1
     
+    # if p_matches[0] > p_matches[1]:
+    #     print("Player 1 wins most of the matches.")
+    # elif p_matches[0] == p_matches[1]:
+    #     print("Match draw")
+    # else:
+    #     print("Player 2 wins most of the matches.")
+    # print(p_matches)
