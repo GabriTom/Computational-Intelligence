957e89b2cd25060013e6e7043d92a62aa48382ac
GabriTom <gabriele.tomatis99@gmail.com>
2024-01-09 19:05:31 +0100

    Minor changes

--- Quixo/main.py
+++ Quixo/main.py
@@ -1,11 +1,12 @@
 import random
+import numpy as np
 from game import Game, Move, Player
 
 #Strategies:
 #    - Random : already implemented
 #    - Optimal
 #    - Mathematical
-#    - Evolutionary
+#    - RL
 
 class RandomPlayer(Player):
     def __init__(self) -> None:
@@ -16,14 +17,18 @@ class RandomPlayer(Player):
         move = random.choice([Move.TOP, Move.BOTTOM, Move.LEFT, Move.RIGHT])
         return from_pos, move
 
-# class OptimalPlayer(Player):
-#     def __init__(self) -> None:
-#         super().__init__()
+class OptimalPlayer(Player):
+    def __init__(self) -> None:
+        super().__init__()
 
-#     def make_move(self, game: 'Game') -> tuple[tuple[int, int], Move]:
-#         from_pos = (random.randint(0, 4), random.randint(0, 4))
-#         move = random.choice([Move.TOP, Move.BOTTOM, Move.LEFT, Move.RIGHT])
-#         return from_pos, move
+    def make_move(self, game: 'Game') -> tuple[tuple[int, int], Move]:
+        #Check su righe e colonne, sommi, fai mossa in quella che esce puteggio più altoù
+        row_max_idx = np.argmax(np.sum(game.get_board(), axis = 1))
+        col_max_idx = np.argmax(np.sum(game.get_board(), axis = 0))
+        print(col_max_idx, " ", row_max_idx)
+        from_pos = (random.randint(0, 4), random.randint(0, 4))
+        move = random.choice([Move.TOP, Move.BOTTOM, Move.LEFT, Move.RIGHT])
+        return from_pos, move
 
 # class MathematicalPlayer(Player):
 #     def __init__(self) -> None:
@@ -41,7 +46,7 @@ if __name__ == '__main__':
     # TO DO playerone must be vector of strategies
     # TO DO 2 then chnage player2 have see if something happens
     player1 = RandomPlayer()
-    player2 = RandomPlayer()
+    player2 = OptimalPlayer()
     winner = g.play(player1, player2)
     g.print()
     print(f"Winner: Player {winner}")
